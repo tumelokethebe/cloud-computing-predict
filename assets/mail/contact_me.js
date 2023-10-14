b612db10552bf6845fb19e9f72254480b2a1ebb4
tumelokethebe <tumelokethebe.d@gmail.com>
2023-10-14 21:19:47 +0200

    API Gateway

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://7s3qmrd0bi.execute-api.eu-west-1.amazonaws.com}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
