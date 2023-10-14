34fa4d343974564ea81e4eefa561f2304ef889c6
tumelokethebe <tumelokethebe.d@gmail.com>
2023-10-14 21:27:30 +0200

    testing api gateway

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://7s3qmrd0bi.execute-api.eu-west-1.amazonaws.com}",
+                url: "https://7s3qmrd0bi.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
